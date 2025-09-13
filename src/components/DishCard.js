ad7e9539c5f44599bdb7451e910ec14efb544fd6
AshwinKanth <ashwinvjrj@gmail.com>
2025-09-13 15:07:08 +0530

    Modified Dish Card Styling

--- src/components/DishCard.js
+++ src/components/DishCard.js
@@ -1,62 +1,55 @@
 import React from "react";
 import "./DishCard.css";
-import { imageMap } from "../data/imageMap";
 import { FaPlus } from "react-icons/fa";
 import { LuSquareDot } from "react-icons/lu";
+import { imageMap } from "../data/imageMap";
+import notFoundImage from "../assets/not-found-image.jpg";
 
-
-const fallbackImage =
-  "https://images.unsplash.com/photo-1565299624946-b28f40a0ca4b?w=200&h=200&fit=crop&crop=center";
 
 const DishCard = ({ dish, isSelected, onAddDish, onRemoveDish, onViewIngredients }) => {
-  const getImage = (id) => imageMap[id] || fallbackImage;
+  const getImage = (id) => imageMap[id] || notFoundImage;
 
   return (
     <div className="dish-card">
-      <div className="dish-content">
-        {/* Dish Image */}
-        <div className="dish-image-container">
-          <div className="dish-image-wrapper">
-            <img
-              src={dish.image || getImage(dish.id)}
-              alt={dish.name}
-              className="dish-image"
-              onError={(e) => (e.target.src = fallbackImage)}
-            />
-          </div>
+      {/* LEFT: details */}
+      <div className="dish-left">
+        <div className="dish-name-row">
+          <h3 className="dish-name" title={dish.name}>{dish.name}</h3>
+          <LuSquareDot
+            className="diet-indicator"
+            size={14}
+            color={dish.type === "VEG" ? "#16a34a" : "#ef4444"}
+            aria-label={dish.type === "VEG" ? "veg" : "non-veg"}
+          />
         </div>
 
-        {/* Dish Details */}
-        <div className="dish-details">
-          <div className="dish-header">
-            <div className="dish-info">
-              <div className="dish-name-row">
-                <h3 className="dish-name">{dish.name}</h3>
-                <LuSquareDot
-                  className="diet-indicator"
-                  color={dish.type === "VEG" ? "green" : "red"}
-                />
-              </div>
-              <p className="dish-description">{dish.description}</p>
-              <button className="ingredient-button" onClick={() => onViewIngredients(dish)}>
-                🥘 Ingredient
-              </button>
-            </div>
+        <p className="dish-description">
+          {dish.description}
+        </p>
 
-            {/* Add/Remove Button */}
-            <div className="action-button-container">
-              {isSelected ? (
-                <button className="remove-button" onClick={() => onRemoveDish(dish.id)}>
-                  Remove
-                </button>
-              ) : (
-                <button className="add-button" onClick={() => onAddDish(dish.id)}>
-                  <FaPlus className="plus-icon" />
-                  Add
-                </button>
-              )}
-            </div>
-          </div>
+        <button className="ingredient-button" onClick={() => onViewIngredients(dish)}>
+          🍲 Ingredient
+        </button>
+      </div>
+
+      {/* RIGHT: image + overlay button */}
+      <div className="dish-right">
+        <div className="dish-image-wrapper">
+          <img
+            // src={imageSrc}
+            src={dish.image || getImage(dish.id)}
+            alt={dish.name}
+            className="dish-image"
+            onError={(e) => (e.target.src = notFoundImage)}
+          />
+
+          <button
+            className={`image-action-pill ${isSelected ? "remove" : "add"}`}
+            onClick={() => (isSelected ? onRemoveDish(dish.id) : onAddDish(dish.id))}
+            aria-pressed={isSelected}
+          >
+            {isSelected ? "Remove" : (<>Add <FaPlus className="pill-plus" /> </>)}
+          </button>
         </div>
       </div>
     </div>
